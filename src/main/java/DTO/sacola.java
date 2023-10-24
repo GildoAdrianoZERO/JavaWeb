32ff0236a8eae4199919147e45b1c150fff56190
Lucas Mello22 <lucas.s.mello@outlook.com>
2023-10-24 14:13:58 -0300

    add class sacola and conecction

---
+++ src/main/java/DTO/sacola.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+/**
+ *
+ * @author Lucas
+ */
+public class sacola {
+       private String nome, descricao;
+       private int qtd;
+       private double preco;
+       
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getDescricao() {
+            return descricao;
+        }
+
+        public void setDescricao(String telefone) {
+            this.descricao = descricao;
+        }
+
+        public int getQtd() {
+            return qtd;
+        }
+
+        public void setQtd(int qtd) {
+            this.qtd = qtd;
+        }
+
+        public double getPreco() {
+            return preco;
+        }
+
+        public void setPreco(double preco) {
+            this.preco = preco;
+        }
+}
